ebd2aff562505d23d4d96807b3824ea0b5158fa9
arupdawn <rupchandradawn@gmail.com>
2024-10-21 19:13:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [taskName, setTaskName] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  useEffect(() => {
+    if (localStorage.getItem("maintask")) {
+      let tasks = JSON.parse(localStorage.getItem("maintask"));
+      if (tasks && tasks?.length > 0) {
+        setTaskList(tasks);
+      }
+    }
+  }, []);
+
+  const onChangeTaskName = (e) => {
+    setTaskName(e.target.value);
+  };
+
+  const onClickAddTask = () => {
+    let tasks = [...taskList];
+    tasks.push(taskName);
+    setTaskName("");
+    setTaskList(tasks);
+    localStorage.setItem("maintask", JSON.stringify(tasks));
+  };
+
+  const onClickEditTask = (taskindex) => {
+    let taskVal = taskList[taskindex];
+    let updatedTaskVal = prompt("Enter new value", taskVal);
+    let newTask = [...taskList];
+    newTask[taskindex] = updatedTaskVal;
+    setTaskList(newTask);
+    localStorage.setItem("maintask", JSON.stringify(newTask));
+  };
+
+  const onClickDeleteTask = (taskindex) => {
+    let newTask = [...taskList];
+    newTask.splice(taskindex, 1);
+    setTaskList(newTask);
+    localStorage.setItem("maintask", JSON.stringify(newTask));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: "center" }}>
+      <h1>To Do App</h1>
+
+      <input
+        type="text"
+        placeholder="Enter your task"
+        value={taskName}
+        onChange={onChangeTaskName}
+        className="input-listtext-style"
+      />
+      <button onClick={onClickAddTask}>Add</button>
+
+      {/* List view */}
+
+      {taskList && taskList?.length > 0 && (
+        <div className="task-list-container">
+          {taskList.map((eachTask, index) => (
+            <div id={index} className="each-task-container">
+              <div>
+                {`${index + 1}.) `}
+                {eachTask}
+              </div>
+              <div>
+                <button onClick={() => onClickEditTask(index)}>Edit</button>
+                <button onClick={() => onClickDeleteTask(index)}>Delete</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
